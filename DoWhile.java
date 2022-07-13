84f33fb1cf71af2e7f58a09c27985e301505d181
Your Name <you@example.com>
2022-07-13 01:05:40 -0300

    first commit

---
+++ DoWhile.java
@@ -0,0 +1,25 @@
+package dowhile;
+
+public class DoWhile {
+
+    public static void main(String[] args) {
+    
+    int i = 1;
+    int max = 10;
+    
+        System.out.println("Contando ate" + max);
+        
+        while (i <= max){
+            System.out.println("Valor de i: " + i);
+            i++;
+        }
+        System.out.println(i);
+        
+        do{
+            i++;
+            System.out.println("Valor de i: " + i);
+        } while (i < 20);
+        System.out.println(i);
+    }
+    
+}
